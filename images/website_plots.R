a1aa3dce7b5d0b060d9ba4af4df284c84439a0d3
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-03-26 10:57:16 -0700

    Update pubs.png
    resize

--- images/website_plots.R
+++ images/website_plots.R
@@ -7,9 +7,9 @@ data.frame(
   total = c(1,0,4,5,5,4,0,1)
 ) %>% 
 
-  ggplot(aes(y=total,x=year))+
+  ggplot(aes(y=total,x=year)) +
   geom_col(fill="#6a51a3") +
-  theme_classic(base_size = 15) +
-  labs(x="",y="Publications") +
-  ggsave("images/pubs.png", width=6, height=3)
+  theme_classic() +
+  labs(x="",y="") +
+  ggsave("images/pubs.png", width=8, height=1.2)
   
