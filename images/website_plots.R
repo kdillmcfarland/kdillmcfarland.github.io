8324cd68e1eae92e70dce1fa92f1a62c644a6850
Kimberly Dill-McFarland <kdillmcfarland@gmail.com>
2023-04-12 12:33:04 -0700

    Update pubs image

--- images/website_plots.R
+++ images/website_plots.R
@@ -1,10 +1,11 @@
+setwd("~/Documents/GitHub/kdillmcfarland.github.io/images")
 library(tidyverse)
 
 #### Pubs per year ####
 
-data.frame(
-  year = as.character(2014:2021),
-  total = c(1,0,4,5,5,4,0,1)
+p1 <- data.frame(
+  year = as.character(2014:2023),
+  total = c(1,0,4,5,5,4,0,1,5,4)
 ) %>% 
 
   ggplot(aes(y=total,x=year)) +
@@ -13,6 +14,7 @@ data.frame(
   theme(text=element_text(color="#bdbdbd"),
         axis.text.x=element_text(color="#bdbdbd"),
         axis.text.y=element_text(color="#bdbdbd")) + 
-  labs(x="",y="") +
-  ggsave("images/pubs.png", width=8, height=1.6)
+  labs(x="",y="")
+
+ggsave(p1, filename="pubs.png", width=8, height=1.6)
   
